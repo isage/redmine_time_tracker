99b0ea3e22e01e507e42dd3d987fe8d8c458e45a
iSage <isage.dna@gmail.com>
2012-03-07 18:58:50 +0400

    proper (finally) update of trackers page

--- assets/javascripts/time_tracker.js
+++ assets/javascripts/time_tracker.js
@@ -5,3 +5,26 @@ function updateElementIfChanged(id, newContent) {
     el = $(id);
     if (el.innerHTML != newContent) { el.update(newContent); }
 }
+
+function updateTrackersStatus(paused_str, running_str, id, running)
+{
+    $$('.tracker_status_row').each( function(elem) { 
+        elem.update(paused_str);
+    });
+    if (!running)
+    {
+        $$('.tracker_start').each(Element.hide);
+        $$('.tracker_pause').each(Element.show);
+        $('tracker_start_' + id).show();
+        $('tracker_suspend_' + id).hide();
+
+    }
+    else
+    {
+        $('timetracker_status_' + id).update(running_str);
+        $$('.tracker_start').each(Element.show);
+        $$('.tracker_pause').each(Element.hide);
+        $('tracker_start_' + id).hide();
+        $('tracker_suspend_' + id).show();
+    }
+}
