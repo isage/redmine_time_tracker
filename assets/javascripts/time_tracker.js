1314b5147762ab5d560168109b777beba6d987b7
iSage <isage.dna@gmail.com>
2012-03-07 20:53:24 +0400

    properly update trackers page when stopping/starting timer via embed menu

--- assets/javascripts/time_tracker.js
+++ assets/javascripts/time_tracker.js
@@ -8,23 +8,25 @@ function updateElementIfChanged(id, newContent) {
 
 function updateTrackersStatus(paused_str, running_str, id, running)
 {
-    $$('.tracker_status_row').each( function(elem) { 
-        elem.update(paused_str);
-    });
-    if (!running)
+    if ($$('.tracker_status_row')!='')
     {
-        $$('.tracker_start').each(Element.show);
-        $$('.tracker_pause').each(Element.hide);
-        $('tracker_start_' + id).show();
-        $('tracker_suspend_' + id).hide();
-
-    }
-    else
-    {
-        $('timetracker_status_' + id).update(running_str);
-        $$('.tracker_start').each(Element.show);
-        $$('.tracker_pause').each(Element.hide);
-        $('tracker_start_' + id).hide();
-        $('tracker_suspend_' + id).show();
+        $$('.tracker_status_row').each( function(elem) { 
+            elem.update(paused_str);
+        });
+        if (!running)
+        {
+            $$('.tracker_start').each(Element.show);
+            $$('.tracker_pause').each(Element.hide);
+            $('tracker_start_' + id).show();
+            $('tracker_suspend_' + id).hide();
+        }
+        else
+        {
+            $('timetracker_status_' + id).update(running_str);
+            $$('.tracker_start').each(Element.show);
+            $$('.tracker_pause').each(Element.hide);
+            $('tracker_start_' + id).hide();
+            $('tracker_suspend_' + id).show();
+        }
     }
 }
