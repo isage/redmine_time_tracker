db367d3353aeebadb5d868099ed5747d55781337
Ivan Epifanov <isage.dna@gmail.com>
2014-02-27 14:55:17 +0400

    fix js

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -1,15 +1,8 @@
-function updateAfterDelete(event, data, status, xhr){
-  console.log(event.target.id);
-  console.log(event.target.className);
-  $(this).html('<td colspan="5" class="msg">' + data + '</td>');
-  updateMenu();
-};
-
 $(document).ready(function(){
-  $("[id^=user-timetracker]").each(function(){
-    $(this).on("ajax:success", updateAfterDelete);
-    console.log(this);
-  });
+//  $("[id^=user-timetracker]").each(function(){
+//    $(this).on("ajax:success", updateAfterDelete);
+//    console.log(this);
+//  });
   
   $("[id^=all-timetracker]").each(function(){
     $(this).on("ajax:success", updateAfterDelete);
